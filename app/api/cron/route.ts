726a50fa0b707f5c0bddbde681e855117f9c800e
Sarthak <saryatnam22@gmail.com>
2024-02-25 22:55:34 +0530

    cache added

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -7,8 +7,9 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
 // export const maxDuration = 10; // This function can run for a maximum of 10 seconds
-export const dynamic = "force-static";
-export const revalidate = 0;
+export const dynamic = "force-dynamic";
+export const revalidate = 1;
+export const fetchCache = 'default-cache';
 
 export async function GET(request: Request) {
   try {
